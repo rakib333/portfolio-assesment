edb8c7983f5942b48f7530fcb854467eabc9beff
Md. Habibullah <79191117+rakib333@users.noreply.github.com>
2023-02-16 08:56:51 +0600

    smooth scroll added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,56 @@
+// smooth scrolling function
+
+function smoothScroll(target, duration) {
+  let setTarget = document.querySelector(target);
+  let targetPosition = setTarget.getBoundingClientRect().top;
+  let startPosition = window.pageYOffset;
+  let distance = targetPosition - startPosition;
+  let startTime = null;
+
+  function animation(currentTime) {
+    if (startTime === null) {
+      startTime = currentTime;
+    }
+    let timeElapsed = currentTime - startTime;
+    const run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) {
+      requestAnimationFrame(animation);
+    }
+  }
+
+  function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return c / 2 * t * t + b;
+      t--;
+      return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+const about = document.querySelector("#about");
+const portfolio = document.querySelector("#portfolio");
+const testimonial = document.querySelector("#testimonial");
+const contact = document.querySelector("#contact");
+
+about.addEventListener("click", function () {
+  smoothScroll("#about-section", 1500);
+});
+portfolio.addEventListener("click", function () {
+  smoothScroll("#portfolio-section", 2000);
+});
+testimonial.addEventListener("click", function () {
+  smoothScroll("#about-section", 1500);
+});
+contact.addEventListener("click", function () {
+  smoothScroll("#about-section", 1500);
+});
+
 // filter the projects
-document.getElementById('filter-btn').addEventListener('click', filterNames);
+document.getElementById("filter-btn").addEventListener("click", filterNames);
 function filterNames() {
-    // get the value of input
-    const getInput = document.getElementById('filter-input').value;
-    
-    const searchItems = document.querySelectorAll('#filter-title');
-    const cards = document.querySelectorAll('.card');
-    searchItems.forEach((element, index) => {
-        if (element.innerText.includes(getInput.toUpperCase())) {
-            cards[index].classList.remove('hide');
-        }
-        else {
-            cards[index].classList.add('hide');
-        }
-    })
+  let filterArray = [];
+  // get the value of input
+  const getInput = document.getElementById("filter-input").value;
 }
